3e5f269d73c4e71e6adec08474192fb78a586005
Ashish-chanchal <akchanchal2002@gmail.com>
2024-07-20 11:38:23 +0530

    javadsa started

---
+++ sumnatural.js
@@ -0,0 +1,13 @@
+
+
+function sum(n){
+    // let sum=0;
+    // for(let i=1;i<=n;i++){
+    //     sum=sum+i;
+    // }
+    // return sum;
+    return n*(n+1)/2;
+}
+
+let allsum =sum(5);
+console.log(allsum);
